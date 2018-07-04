31d9d0a1ebed5a2e7e42e45e59e6dac0ece8120d
KorolPaul <korol.paul@gmail.com>
2018-07-04 22:22:31 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () { 
+$(window).on('load', function () { 
 
     /* Menu toggle */
     $('.menu-toggle').on('click', function(e) {
@@ -6,7 +6,7 @@ $(document).ready(function () {
         $(this).next().toggleClass('opened');
     });
 
-    $('.menu_list-item__parent a').on('click', function(e) {
+    $('.menu').on('click', '.menu_list-item__parent > a', function(e) {
         e.preventDefault();
         $(this).next().toggleClass('opened');
     });
@@ -19,7 +19,7 @@ $(document).ready(function () {
     });
 
     /* Tabs */
-    $(".tabs_button").on('click', function(e) {
+    $(".tabs").on('click', 'button', function(e) {
         e.preventDefault();
 
         $(".tabs_button.active, .tabs_tab.active").removeClass('active');
@@ -29,30 +29,23 @@ $(document).ready(function () {
     });
 
     /*Range inputs*/
-    $("#price").slider({
-        min: 10,
-        max: 100,
-        range: true
-    }).on('slide', function(e) {
-        $(this).parent().find('.value-1').html(e.value[0]);
-        $(this).parent().find('.value-2').html(e.value[1]);
-    });
-    $("#weight").slider({
-        min: 10,
-        max: 2000,
-        range: true
-    }).on('slide', function(e) {
-        $(this).parent().find('.value-1').html(e.value[0]);
-        $(this).parent().find('.value-2').html(e.value[1]);
-    });
-
-    /* Tabs */
-    $('.tab-button').on('click', function() {
-        $('.tab-button').removeClass('active');
-        $('div[data-tab]').removeClass('opened');
-
-        $(this).addClass('active');
-        $('div[data-tab="' + $(this).data('tab') + '"]').addClass('opened');
-    });
+    if ($("#price").length) {
+        $("#price").slider({
+            min: 10,
+            max: 100,
+            range: true
+        }).on('slide', function(e) {
+            $(this).parent().find('.value-1').html(e.value[0]);
+            $(this).parent().find('.value-2').html(e.value[1]);
+        });
+        $("#weight").slider({
+            min: 10,
+            max: 2000,
+            range: true
+        }).on('slide', function(e) {
+            $(this).parent().find('.value-1').html(e.value[0]);
+            $(this).parent().find('.value-2').html(e.value[1]);
+        });
+    }
 
  });
